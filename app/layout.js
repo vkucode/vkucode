7db6a39efd5e5f891a76c4fc87613ac0cbc3d202
VKU CODE <ungurean729@gmail.com>
2024-09-01 21:43:33 +0200

    some little aditions

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "VKU CODE",
   description: "",
+  theme: "#0a0a0f",
 };
 
 export default function RootLayout({ children }) {
