df1b5871445c27ea8b0a0d073ef7b57c457c2d57
VKU CODE <ungurean729@gmail.com>
2024-09-03 19:42:24 +0200

    Update page.jsx

--- app/love/page.jsx
+++ app/love/page.jsx
@@ -12,19 +12,19 @@ export default function Love() {
         const Writer = new GlitchedWriter('#glitch_this', { letterize: true }, logString);
         
         const initWriter = async () => {
-            await wait(1200);
+            await wait(1500);
             await Writer.write("Я знаю, это кажется странным");
             
-            await wait(1500);
+            await wait(2000);
             await Writer.write("но я хотел сказать тебе");
             
-            await wait(2000);
+            await wait(2500);
             await Writer.write("что думаю о тебе все время мой ангелочек");
 
-            await wait(2500);
+            await wait(3000);
             await Writer.write("Я очень благодарен Господу, что ты у меня есть");
 
-            await wait(3000);
+            await wait(3500);
             await Writer.write("и хочу сказать, что я очень сильно тебя люблю");
             
             setTimeout(() => {
