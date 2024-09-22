ae1d3555d05cd50402575ee0a4818315eb7f94fd
VKU CODE <ungurean729@gmail.com>
2024-09-22 19:46:51 +0200

    update on customers component

--- app/components/customers/Customers.jsx
+++ app/components/customers/Customers.jsx
@@ -19,13 +19,12 @@ export default function Customers(){
 
   return (
     <>
-    <section id="testimonials" className="relative overflow-hidden">
+    <section id="testimonials" className="relative overflow-hidden bg-zinc-950">
       <MaxWidthWrapper className="relative z-10 py-20 md:py-40">
         <SectionHeadingSpacing>
           <SectionHeading>Les avis de nos <span className="text-purple-500">clients !</span></SectionHeading>
           <SectionSubheading>
-            People trust people. Show off all of the great reviews of your
-            product to ease the minds of potential customers.
+            Découvrez ce disent nos clients sur la qualité, l'innovation et le respect des délais qui font la force de notre équipe.
           </SectionSubheading>
         </SectionHeadingSpacing>
         <motion.div
@@ -99,20 +98,20 @@ const ReviewsColumn = ({ reviews }) => {
             <div className="relative flex items-center gap-2 py-2 text-xs">
               <img
                 src={r.src}
-                alt={`Placeholder image for faux user ${r.name}`}
+                width={200}
+                height={200}
+                alt={`image client ${r.name}`}
                 className="size-8 rounded-full"
               />
               <div>
                 <span className="block font-medium text-zinc-300">
                   {r.name}
                 </span>
-                <span className="block text-zinc-500">{r.handle}</span>
               </div>
             </div>
 
-            <SiX className="text-sky-300" />
           </div>
-          <p>{r.review}</p>
+          <p className="text-white">{r.review}</p>
         </Card>
       ))}
     </div>
@@ -122,91 +121,92 @@ const ReviewsColumn = ({ reviews }) => {
 const REVIEWS = {
   left: [
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=John",
-      name: "John Johnson",
+      src: "/img/accueil/clients/larbre.ico",
+      name: "L'arbre a Pains",
       handle: "@johns6",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum labore incidunt temporibus sunt? Excepturi nulla atque odio dolore, velit harum.",
+        "Nous sommes extrêmement satisfaits des services offerts par cette entreprise pour la création de notre site web. Les délais ont été respectés, et les technologies utilisées sont à la pointe de l'innovation. Un travail impeccable!",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Dan",
-      name: "Dan Daniels",
+      src: "/img/accueil/clients/jby.png",
+      name: "JBY Renovation",
       handle: "@DannyD",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum labore velit harum.",
+        "Une prestation de haute qualité! Notre site est moderne et répond parfaitement à nos besoins. Les délais ont été respectés, et le service a été excellent du début à la fin.",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Phil",
-      name: "Phil Phillips",
+      src: "/img/accueil/clients/optilog.png",
+      name: "Optilog",
       handle: "@phillipsp55104",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum labore incidunt temporibus sunt? Excepturi velit harum.",
+        "Le travail réalisé sur notre site web dépasse nos attentes. L’équipe a utilisé des technologies innovantes, respecté les délais et nous a fourni un site de qualité supérieure.",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Andy",
-      name: "Andy Anderson",
+      src: "/img/accueil/clients/ehc.png",
+      name: "Groupe EHC",
       handle: "@andyandyandy",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing. Excepturi nulla atque odio dolore, velit harum.",
+        "L'expérience avec cette entreprise a été excellente. Ils ont respecté les délais et livré un site web qui utilise les dernières technologies. Très satisfait!",
     },
   ],
   center: [
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Steve",
-      name: "Steve Stevenson",
+      src: "/img/accueil/clients/excellence.png",
+      name: "Excellence BTP",
       handle: "@steves",
-      review: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+      review:
+        "Un service rapide et de qualité. Le site web est à la hauteur de nos attentes, et les délais ont été parfaitement respectés.",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Peter",
-      name: "Peter Peterson",
+      src: "/img/accueil/clients/bills.png",
+      name: "Bill's Burger",
       handle: "@petey.pete",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum labore incidunt.",
+        "Une équipe professionnelle qui a su répondre à toutes nos attentes. Le site livré est moderne, innovant, et respectueux des délais impartis.",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Paul",
-      name: "Paul Paulson",
+      src: "/img/accueil/clients/platane.png",
+      name: "Platane Pizza",
       handle: "@p.paulson44",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit velit harum.",
+        "Des services de grande qualité. Le site livré est rapide, esthétique, et très fonctionnel, tout en respectant les délais annoncés.",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Phil",
-      name: "Andrew Andrews",
+      src: "/img/accueil/clients/ornissa.png",
+      name: "Ornissa",
       handle: "@andrewA",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum labore incidunt temporibus sunt.",
+        "Un excellent partenariat pour la création de notre site web. Le travail a été fait en temps voulu, avec des technologies modernes et une approche très professionnelle.",
     },
   ],
   right: [
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Jeff",
-      name: "Jeff Jefferson",
+      src: "/img/accueil/clients/perdamaian.png",
+      name: "Perdamaian",
       handle: "@jeffJ99",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi nulla atque odio dolore, velit harum.",
+        "Je recommande vivement cette entreprise! Le projet a été réalisé dans les délais avec un souci constant de qualité et d'innovation. Très satisfait du résultat final.",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Jess",
-      name: "Jess Jessica",
+      src: "/img/accueil/clients/like-up.png",
+      name: "Like-Up",
       handle: "@jess",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum labore velit harum. Dolor sit amet consectetur adipisicing elit!",
+        "Une expérience formidable du début à la fin. Le site livré est conforme à nos attentes et utilise des technologies de pointe. Les délais ont été respectés.",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Ben",
-      name: "Ben Benjamin",
+      src: "/img/accueil/clients/delta.png",
+      name: "Delta Invested",
       handle: "@banjamin4491",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum labore incidunt temporibus sunt? Excepturi velit harum.",
+        "L'équipe a respecté les délais et nous a fourni un site web de grande qualité. Les technologies utilisées sont modernes et efficaces. Nous sommes très contents du résultat.",
     },
     {
-      src: "https://api.dicebear.com/7.x/avataaars/svg?seed=Frank",
-      name: "Frank Franklin",
+      src: "/img/accueil/clients/contaexact.png",
+      name: "Contaexact",
       handle: "@frankyf00",
       review:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing. Excepturi nulla.",
+        "Un service rapide, de qualité et respectueux des délais. Le site livré est moderne et très bien conçu. Merci pour ce travail impeccable!",
     },
   ],
 };
