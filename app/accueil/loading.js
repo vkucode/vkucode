21858b7904ba4fd66b194905fab8daf44e0b4c55
VKU CODE <ungurean729@gmail.com>
2024-09-23 22:41:11 +0200

    loading update

---
+++ app/accueil/loading.js
@@ -0,0 +1,17 @@
+export default function Loading() {
+  return (
+    <>
+      <section className="bg-[#0D0D0E] w-screen h-dvh flex flex-col justify-center items-center">
+        <video
+          className="w-full bg-transparent max-w-[150px] lg:max-w-[350px]"
+          src="/video/loading.mp4"
+          poster="/img/logoVKU.png"
+          autoPlay
+          muted
+          loop
+          style={{ backgroundColor: "transparent" }} // Stil adăugat pentru siguranță
+        ></video>
+      </section>
+    </>
+  );
+}
