91b3def2229df22a4bbf175b045c2e8d163a679a
VKU CODE <ungurean729@gmail.com>
2024-11-05 11:18:53 +0100

    Update page.jsx

--- app/accueil/page.jsx
+++ app/accueil/page.jsx
@@ -1,5 +1,7 @@
 'use client'
 import styles from './accueil.module.scss'
+import { useEffect, useState } from 'react';
+import Loading from './loading';
 import HeaderAccueil from './components/Header'
 import SmartDevelopment from './components/SmartDevelopment'
 import GalleryAccV2 from './components/GalleryAccV2'
@@ -9,6 +11,20 @@ import Navbar from '../components/Navbar'
 import CustomersSection from '../components/customers/CustomersSection'
 
 export default function Accueil() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulăm încărcarea tuturor componentelor
+        const loadComponents = setTimeout(() => {
+            setLoading(false);
+        }, 1000); // Așteaptă 1 secundă sau ajustează timpul dorit
+
+        return () => clearTimeout(loadComponents);
+    }, []);
+
+    if (loading) {
+        return <Loading />;
+    }
     return (
         <>
             <section className={styles.accueilPage}>
