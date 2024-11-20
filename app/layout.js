e8734e1f539fe5f5d19d46a1dca8dca5feb8d964
VKU CODE <ungurean729@gmail.com>
2024-11-20 19:13:36 +0100

    update the metadata on main page

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,25 @@ export const metadata = {
   title:
     "VKU CODE - Expert en Création de Sites Web sur Mesure & Cybersécurité Avancée",
   description:
-    "VKU CODE, votre partenaire IT pour la création de sites web sur mesure, performants et sécurisés. Spécialistes en conception web personnalisée et en cybersécurité avancée, nous offrons des solutions numériques innovantes pour protéger et développer votre entreprise en ligne.",
+    "Découvrez les solutions digitales sur mesure de VKU CODE : développement de sites web, applications mobiles et intégration IT pour répondre aux besoins des startups et entreprises. Faites le choix de la performance et de l'innovation.",
   keywords:
     "création de sites web, conception web personnalisée, cybersécurité avancée, solutions IT, développement web, sécurité informatique, VKU CODE, sites web sécurisés, services IT, design web sur mesure",
+  openGraph: {
+    title:
+      "VKU CODE - Expert en Création de Sites Web sur Mesure & Cybersécurité Avancée",
+    description:
+      "Découvrez les solutions digitales sur mesure de VKU CODE : développement de sites web, applications mobiles et intégration IT pour répondre aux besoins des startups et entreprises. Faites le choix de la performance et de l'innovation.",
+    type: "website",
+    url: "https://vkucode.com/", // URL de la page
+    images: [
+      {
+        url: "https://vkucode.com/images/original.jpg", // Remplacez par l'URL de votre image
+        width: 1200,
+        height: 630,
+        alt: "Solutions digitales adaptées aux entreprises par VKU CODE",
+      },
+    ],
+  },
   icons: {
     icon: "/img/favicon.ico",
     shortcut: "/img/favicon.ico",
